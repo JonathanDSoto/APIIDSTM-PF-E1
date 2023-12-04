d501d67da53bd059a2d5ac8686f2c40fb70137d3
Javierdma <javierestebandominguez@outlook.com>
2023-12-03 19:21:10 -0700

    front, modificacion layouts.head

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,7 +6,6 @@
 
 <meta name="description" content="Start your development with a Dashboard for Bootstrap 5" />
 <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5" />
-<meta name="csrf-token" content="{{ csrf_token() }}">
 <!-- Canonical SEO -->
 <link rel="canonical" href="https://1.envato.market/vuexy_admin" />
 
@@ -69,3 +68,7 @@
 <script src="../../assets/vendor/js/template-customizer.js"></script>
 <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 <script src="../../assets/js/config.js"></script>
+
+
+{{-- Axios --}}
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
