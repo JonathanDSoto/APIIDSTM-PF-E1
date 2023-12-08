76ab2076d831b41cd515abfa4119843fe6655439
Kakura1 <joseramonv_20@alu.uabcs.mx>
2023-12-08 14:05:11 -0700

    Actualizacion de Controladores y Rutas - Backend

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -24,14 +24,6 @@ class CategoryController extends Controller
         return redirect()->route('categories.index');
     }
 
-    public function create(Request $request){ // Redirige a la vista de crear una marca
-        $trademark = new Trademark();
-        $trademark->marca = $request->name;
-        $trademark->image = $request->image;
-        $trademark->save();
-        return redirect()->route('categories.index');
-    }
-
     public function update(Request $request,  $id){ // Actualiza una categoria en específico y regresa a la vista con la categoria actualizada
         Category::where('id', $id)->update([
             'categoria' => $request->categoria,
@@ -40,23 +32,9 @@ class CategoryController extends Controller
         return redirect()->route('categories.index');
     }
 
-    public function update_trademark(Request $request,  $id){ // Actualiza una categoria en específico y regresa a la vista con la categoria actualizada
-        Trademark::where('id', $id)->update([
-            'marca' => $request->marca,
-            'image' => $request->image,
-        ]);
-
-        return redirect()->route('categories.index');
-    }
-
     public function destroy($id){ // Elimina una categoria en específico
         $category = Category::findOrFail($id);
         $category->delete();
-        return redirect()->route('categories.index');
-    }
-    public function destroy_trademark($id){ // Elimina una categoria en específico
-        $trademark = Trademark::findOrFail($id);
-        $trademark->delete();
         return redirect()->route('categories.index');
     }
 }
